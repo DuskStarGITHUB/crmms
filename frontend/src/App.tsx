3063ffddce2557cee6b6150b2ec5da44e786c89b
DuskStarGITHUB <contactspeencer@gmail.com>
2025-08-21 13:55:59 -0600

    fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,8 @@
  */
 
 // ROUTERS
-import ModulesRouter from "./routers/modulesRouter.tsx";
+import ModulesRouter from "@/routers/modulesRouter";
+import Example from "@/components/app/example";
 
 // DEPLOY
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
       <div className="w-full h-screen flex flex-col justify-center items-center">
         <h1 className="text-3xl">HELLO!</h1>
         <p className="mt-2">VITE||REACT||TS||TAILWIND|SHADCN && LUCIDE-REACT</p>
+        <Example />
       </div>
       <ModulesRouter />
     </>
--- frontend/src/components/app/example.tsx
+++ frontend/src/components/app/example.tsx
@@ -4,3 +4,13 @@
  *  DESCRIPTION: example directory
  * =====================================================
  */
+
+import { Button } from "@/components/ui/button";
+
+export default function Example() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
