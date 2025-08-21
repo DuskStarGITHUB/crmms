263eada701ad97c76c033f279019bfdfc3c87eba
DuskStarGITHUB <contactspeencer@gmail.com>
2025-08-21 17:55:47 -0600

    fix init

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+/**
+ * =====================================================
+ *  NAME    : app.module.ts
+ *  DESCRIPTION: server join modules
+ * =====================================================
+ */
+
+// DEPENDENCIES
+import { Module } from '@nestjs/common';
+import { ServerModule } from './server/server.module';
+
+// MODULES
+@Module({
+  imports: [ServerModule],
+})
+export class AppModule {}
