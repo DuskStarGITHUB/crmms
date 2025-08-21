abcb84ba721a85857367f7c3a49bb53f82c1ef3f
DuskStarGITHUB <contactspeencer@gmail.com>
2025-08-21 13:04:34 -0600

    commentary

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -9,7 +9,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Tailwind
+// TAILWIND
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
