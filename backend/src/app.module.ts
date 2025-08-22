981ca569cf6b52dc3202c1cf4e452db4ab4dd6bc
DuskStarGITHUB <contactspeencer@gmail.com>
2025-08-21 19:57:28 -0600

    JWT download

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,9 +8,10 @@
 // DEPENDENCIES
 import { Module } from '@nestjs/common';
 import { ServerModule } from './server/server.module';
+import { AuthModule } from './auth/auth.module';
 
 // MODULES
 @Module({
-  imports: [ServerModule],
+  imports: [ServerModule, AuthModule],
 })
 export class AppModule {}
